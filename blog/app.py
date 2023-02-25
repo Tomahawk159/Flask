a079679777c5059aa5d2709e34070352656c095e
Viktor <tomahabk59@gmail.com>
2023-02-25 09:39:08 +0500

    add users and articles

--- blog/app.py
+++ blog/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from blog.users.views import user
+from blog.articles.views import article
 
-app = Flask(__name__)
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    register_blueprints(app)
+    return app
 
 
-@app.route('/')
-def index():
-    return 'Hello world'
+def register_blueprints(app: Flask):
+    app.register_blueprint(user)
+    app.register_blueprint(article)
