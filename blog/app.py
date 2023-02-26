d4d3a295edba8540597cb9c1a6db130267271503
Viktor <tomahabk59@gmail.com>
2023-02-26 11:03:31 +0500

    creat_model

--- blog/app.py
+++ blog/app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 from blog.users.views import user
 from blog.articles.views import article
+from blog.models.database import db
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + \
+        os.path.join(basedir, 'blog.db')
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+
     register_blueprints(app)
     return app
 
