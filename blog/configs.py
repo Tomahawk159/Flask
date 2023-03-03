48c91f0639f40fcc8ebd47f5eb70ec2226098170
Viktor <tomahabk59@gmail.com>
2023-03-03 12:24:48 +0500

    add config, migration

---
+++ blog/configs.py
@@ -0,0 +1,22 @@
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class BaseConfig(object):
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'blog.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = "abcdefg123456"
+
+
+class DevConfig(BaseConfig):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
+
+
+class TestingConfig(BaseConfig):
+    TESTING = True
